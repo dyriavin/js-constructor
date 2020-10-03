d682f90aaa464738e86450511e0b2d83dee81ec0
dyriavin <Dyriavin@gmail.com>
2020-10-03 15:48:05 +0300

    parcel implemented setup

---
+++ src/model.js
@@ -0,0 +1,12 @@
+export const model = [
+    {type: 'title', value: 'Hello world from JS'},
+    {type: 'text', value: 'here we got text'},
+    {
+        type: 'columns', value: [
+            '1111',
+            '2222',
+            '3333',
+        ]
+    },
+    {type:'image',value: './src/assets/image.png'}
+]
---
+++ src/utils.js
@@ -0,0 +1,6 @@
+export function row(content){
+    return `<div>${content}</div>`
+}
+export function col(content){
+    return `<div>${content}</div>`
+}
